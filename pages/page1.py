bc4af0058c7985d5d3dc0211f0d69231dd43d645
Vincent NOGUERO <vincent.noguero@etu.iut-tlse3.fr>
2023-03-08 16:56:07 +0100

    Vn pas finis

--- pages/page1.py
+++ pages/page1.py
@@ -1,24 +1,32 @@
 import pandas
 import streamlit as st
 import reportingFonction as rF
-
+import plotly.express as px
+import plotly.figure_factory as ff
 col1, col2= st.columns(2)
 
+with st.container():
+    with col1:
+        total1 = rF.count_Collection("ClinicalTrials_ObsStudies")
+        total2 = rF.count_Collection("ClinicalTrials_RandTrials")
+        st.header("Nombre d'essai : "+str(total1+total2))
+        st.subheader("ObsStudies : "+str(total1))
+        st.subheader("RandTrials : "+str(total2))
 
-with col1:
-    total1 = rF.count_Collection("ClinicalTrials_ObsStudies")
-    total2 = rF.count_Collection("ClinicalTrials_RandTrials")
-    st.header("Nombre d'essai : "+str(total1+total2))
-    st.subheader("ObsStudies : "+str(total1))
-    st.subheader("RandTrials : "+str(total2))
+    with col2:
+        total3 = rF.count_Collection("Publications_ObsStudies")
+        total4 = rF.count_Collection("Publications_RandTrials")
+        st.header("Nombre d'essai : "+str(total3 + total4))
+        st.subheader("ObsStudies : "+str(total3))
+        st.subheader("RandTrials : "+str(total4))
 
-with col2:
-    total1 = rF.count_Collection("Publications_ObsStudies")
-    total2 = rF.count_Collection("Publications_RandTrials")
-    st.header("Nombre d'essai : "+str(total1 + total2))
-    st.subheader("ObsStudies : "+str(total1))
-    st.subheader("RandTrials : "+str(total2))
+nbListe = [total1,total2,total3,total4]
+nomListe = ["ClinicalTrials_ObsStudies","ClinicalTrials_RandTrials","Publications_ObsStudies","Publications_RandTrials"]
 
+dataNb = pandas.DataFrame(nbListe,nomListe)
+st.write(dataNb)
+fig = px.pie(dataNb)
+st.plotly_chart(fig)
 
 with st.container():
     data = rF.nb_par_Mois("ClinicalTrials_ObsStudies")
